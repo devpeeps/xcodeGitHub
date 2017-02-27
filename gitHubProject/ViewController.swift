728c7ef21f8b32f49100689ae876bc90f99c1279
erick apostol <apostol@ericks-MacBook-Pro.local>
2017-02-27 23:56:14 +0800

    Initial Commit

---
+++ gitHubProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitHubProject
+//
+//  Created by erick apostol on 27/02/2017.
+//  Copyright © 2017 erick apostol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
